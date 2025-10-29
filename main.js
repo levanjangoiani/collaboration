d5624d9cbbe5e93c40619e0cde86eedc7975dbd5
levanjangoiani <ljangoiani@gmail.com>
2025-10-29 16:29:46 +0400

    test scroll

--- main.js
+++ main.js
@@ -2,3 +2,18 @@
 fetch('https://fakestoreapi.com/products')
 .then(data =>  data.json())
 .then(data => htmlRenderer(data));
+const slider = document.getElementById('slider');
+let scrollAmount = 0;
+
+function autoScroll() {
+  scrollAmount += 1;
+  if (scrollAmount >= slider.scrollWidth - slider.clientWidth) {
+    scrollAmount = 0;
+  }
+  slider.scrollTo({
+    left: scrollAmount,
+    behavior: 'smooth'
+  });
+}
+
+setInterval(autoScroll, 50);
